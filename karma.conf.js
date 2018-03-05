3d123778301816b1eab756124fe530f2bf88284b
Chris Colvard <cjcolvar@indiana.edu>
2018-03-05 14:36:57 -0500

    Use yarn for js tests

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: true
+    singleRun: true,
+    failOnEmptyTestSuite: false
   });
 };
