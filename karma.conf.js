7ae128956ce103a857d38656a640a500395e4c94
Phuong Dinh <phuongdh@gmail.com>
2018-03-09 13:37:51 -0500

    Use CircleCI 2.0

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,13 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['MyHeadlessChrome'],
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-translate', '--disable-extensions']
+      }
+    },
     singleRun: true,
     failOnEmptyTestSuite: false
   });
