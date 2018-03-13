1fd97d5c45c64d9b02af38499f4f1107052c4c37
Adam Arling <adam.arling@northwestern.edu>
2018-03-13 15:54:18 -0500

    Add sample iiif manifest, and functionality to play a source from the manifest in media player

--- app/javascript/packs/avalon-player/index.js
+++ app/javascript/packs/avalon-player/index.js
@@ -1,8 +1,25 @@
+import iiifParser from '../avalon-iiif';
 // Import any custom CSS here
 import './styles/avalon-bundle-styles.css';
+// Import a sample manifest
+import lunchroomManifest from './lunchroom-manners-manifest.json';
+
+const replaceManifestUri = manifest => {
+  const uri = iiifParser(manifest);
+  let el = document.getElementById('mejs');
+
+  if (!el) {
+    return;
+  }
+  el.setAttribute('src', uri);
+};
 
 const initPlayer = () => {
   $('document').ready(() => {
+    // Swap out <video> source with a source parsed from a IIIF manifest
+    replaceManifestUri(lunchroomManifest);
+
+    // Initialize Mediaelement on the media HTML element
     $('#mejs').mediaelementplayer({
       stretching: 'responsive',
       success: function(mediaElement, originalNode, instance) {
