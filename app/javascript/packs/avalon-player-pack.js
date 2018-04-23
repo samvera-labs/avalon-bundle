ae83b269ff2021738970a941524dc02db487b4c9
Adam Arling <adam.arling@northwestern.edu>
2018-04-23 11:26:01 -0500

    Verify Sass files compiled correctly via webpacker

--- app/javascript/packs/avalon-player-pack.js
+++ app/javascript/packs/avalon-player-pack.js
@@ -3,6 +3,6 @@ import 'mediaelement';
 import 'mediaelement/build/mediaelementplayer.css';
 
 // Initialize the player (on DOM load and if there is an element Mediaelement is looking for)
-import initPlayer from './avalon-player';
+import initPlayer from '../avalon-player';
 
 initPlayer();
