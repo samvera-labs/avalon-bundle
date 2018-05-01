0f9cdb703b3490ead7de6905ff52547b70778247
yingfeng-iu <yingfeng@iu.edu>
2018-05-01 16:37:55 -0400

    Issue#101 add AudiovisualWork files generated by rails

---
+++ app/controllers/hyrax/audiovisual_works_controller.rb
@@ -0,0 +1,13 @@
+# Generated via
+#  `rails generate hyrax:work AudiovisualWork`
+module Hyrax
+  # Generated controller for AudiovisualWork
+  class AudiovisualWorksController < ApplicationController
+    # Adds GenericWorks behaviors to the controller.
+    include GenericWorksController
+    self.curation_concern_type = ::AudiovisualWork
+
+    # Use this line if you want to use a custom presenter
+    self.show_presenter = Hyrax::AudiovisualWorkPresenter
+  end
+end
---
+++ spec/forms/hyrax/audiovisual_work_form_spec.rb
@@ -0,0 +1,9 @@
+# Generated via
+#  `rails generate hyrax:work AudiovisualWork`
+require 'rails_helper'
+
+RSpec.describe Hyrax::AudiovisualWorkForm do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
