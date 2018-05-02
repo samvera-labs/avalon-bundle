cdd0fb3f58a8bd45be61ca1a5a37283a2e162001
Adam Arling <adam.arling@northwestern.edu>
2018-05-02 15:34:58 -0500

    Jest unit testing initial patterns for mocking external api calls, and mocking node_modules packages

--- app/javascript/avalon-player/index.js
+++ app/javascript/avalon-player/index.js
@@ -1,4 +1,15 @@
 import AvalonPlayer from './AvalonPlayer';
-import './styles/avalon-bundle-styles';
+import './styles/avalon-bundle-styles.scss';
 
-new AvalonPlayer();
+const avalonPlayer = new AvalonPlayer();
+
+$('document').ready(() => {
+  init();
+});
+
+export async function init() {
+  const mediaId = avalonPlayer.getMediaObjectId();
+  const manifest = await avalonPlayer.getManifest(mediaId);
+  avalonPlayer.setVideoSourceUrls(manifest);
+  avalonPlayer.wrapWithMediaelementPlayer();
+}
