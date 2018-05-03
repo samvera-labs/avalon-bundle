d7636d2dcae9e1395ad18e06bf20994ffc22fbca
Adam Arling <adam.arling@northwestern.edu>
2018-05-03 11:56:20 -0500

    Cleanup webpack test.js file

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,15 +1,7 @@
 // config/webpack/test.js
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
 const environment = require('./environment')
-// environment.plugins.get('Manifest').opts.writeToFileEmit = process.env.NODE_ENV !== 'test'
-// environment.loaders.set('istanbul-instrumenter', {
-//   test: /\.js$/,
-//   enforce: "post",
-//   loader: "istanbul-instrumenter-loader",
-//   query: {
-//     esModules: true
-//   },
-//   exclude: ["node_modules", /\.spec\.js$/]
-// }) // optional
 
 module.exports = environment.toWebpackConfig()
