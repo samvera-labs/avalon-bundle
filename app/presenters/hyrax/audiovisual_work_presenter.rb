a3c60523a85721706aeb902e99eba42b4a7d26fd
Chris Colvard <cjcolvar@indiana.edu>
2018-05-31 15:01:45 -0400

    Pull metadata fields out to constant to satisfy rubocop

--- app/presenters/hyrax/audiovisual_work_presenter.rb
+++ app/presenters/hyrax/audiovisual_work_presenter.rb
@@ -34,20 +34,20 @@ module Hyrax
     delegate :related_item, to: :solr_document
     delegate :note, to: :solr_document
 
+    IIIF_METADATA_FIELDS = [
+      :title, :creator, :rights_statement, :date_issued, :date_created, :contributor, :publisher, :abstract,
+      :physical_description, :language, :genre, :topical_subject, :temporal_subject, :geographic_subject,
+      :permalink, :related_item, :bibliographic_id, :local, :oclc, :lccn, :issue_number, :matrix_number,
+      :music_publisher, :video_recording_identifier, :table_of_contents, :note, :license, :terms_of_use
+    ].freeze
+
     # IIIF metadata for inclusion in the manifest (overrides Hyrax::WorkShowPresenter method)
     #  Called by the `iiif_manifest` gem to add metadata
     #
     # @return [Array] array of metadata hashes
     def manifest_metadata
-      iiif_metadata_fields = [
-        :title, :creator, :rights_statement, :date_issued, :date_created, :contributor, :publisher, :abstract,
-        :physical_description, :language, :genre, :topical_subject, :temporal_subject, :geographic_subject,
-        :permalink, :related_item, :bibliographic_id, :local, :oclc, :lccn, :issue_number, :matrix_number,
-        :music_publisher, :video_recording_identifier, :table_of_contents, :note, :license, :terms_of_use
-      ]
-
       metadata = []
-      iiif_metadata_fields.each do |field|
+      IIIF_METADATA_FIELDS.each do |field|
         value = Array.wrap(send(field))
         next if value.blank?
         metadata << {
