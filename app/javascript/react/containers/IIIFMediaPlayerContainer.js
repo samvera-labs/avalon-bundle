9c99aa6dd17553357f31f7d677ac4babd336f15a
Adam Arling <adam.arling@northwestern.edu>
2018-06-01 08:35:47 -0500

    Configure webpacker for react, and import mediaelement yarn package as a player component

---
+++ app/javascript/react/containers/IIIFMediaPlayerContainer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import IIIFMediaPlayer from 'react-iiif-media-player';
+
+const IIIFMediaPlayerContainer = () => <IIIFMediaPlayer />;
+
+export default IIIFMediaPlayerContainer;
