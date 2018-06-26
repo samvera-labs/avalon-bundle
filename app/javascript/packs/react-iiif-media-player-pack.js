1040bbd77db9c9783d47af912c82ed112f00687a
yingfeng-iu <yingfeng@iu.edu>
2018-06-26 14:19:39 -0400

    Issue#145: fix turbolink:load

--- app/javascript/packs/react-iiif-media-player-pack.js
+++ app/javascript/packs/react-iiif-media-player-pack.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IIIFMediaPlayerContainer from '../react/containers/IIIFMediaPlayerContainer';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   ReactDOM.render(
     <IIIFMediaPlayerContainer />,
     document.getElementById('react-mounter')
