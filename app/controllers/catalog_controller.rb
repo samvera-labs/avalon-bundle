add17a1e1641fdea0801a0bf9b20bbb480f6411a
yingfeng-iu <yingfeng@iu.edu>
2018-07-02 14:50:38 -0400

    Issue#133: updated faceted fields

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -57,15 +57,25 @@ class CatalogController < ApplicationController
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
-    config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
-    config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
+    # config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5  # TODO: issue#133:
+    # config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5 # TODO: issue#133:
+    # config.add_facet_field solr_name("creator", :facetable), limit: 5
+    # config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
+    # config.add_facet_field solr_name("keyword", :facetable), limit: 5 # TODO: issue#133:
+    # config.add_facet_field solr_name("subject", :facetable), limit: 5 # TODO: issue#133:
+    # config.add_facet_field solr_name("language", :facetable), limit: 5
+    # config.add_facet_field solr_name("based_near_label", :facetable), limit: 5  # TODO: issue#133:
+    # config.add_facet_field solr_name("publisher", :facetable), limit: 5 # TODO: issue#133:
+    # config.add_facet_field solr_name("file_format", :facetable), limit: 5
+    # config.add_facet_field solr_name('member_of_collections', :symbol), limit: 5, label: 'Collections'
+    # TODO: issue#133: publication date, unit, published, external group, date digitized, date ingested
+
+    config.add_facet_field solr_name("date_issued", :facetable), limit: 5
+    config.add_facet_field solr_name("date_created", :facetable), limit: 5
     config.add_facet_field solr_name("creator", :facetable), limit: 5
     config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
-    config.add_facet_field solr_name("keyword", :facetable), limit: 5
-    config.add_facet_field solr_name("subject", :facetable), limit: 5
     config.add_facet_field solr_name("language", :facetable), limit: 5
-    config.add_facet_field solr_name("based_near_label", :facetable), limit: 5
-    config.add_facet_field solr_name("publisher", :facetable), limit: 5
+    config.add_facet_field solr_name("genre", :facetable), limit: 5
     config.add_facet_field solr_name("file_format", :facetable), limit: 5
     config.add_facet_field solr_name('member_of_collections', :symbol), limit: 5, label: 'Collections'
 
