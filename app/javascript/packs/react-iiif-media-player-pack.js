d8a4dae2005cc043d988b57818966de8783c6bae
Chris Colvard <cjcolvar@indiana.edu>
2018-07-06 09:43:04 -0400

    Unload react component before turbolinks caches

--- app/javascript/packs/react-iiif-media-player-pack.js
+++ app/javascript/packs/react-iiif-media-player-pack.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IIIFMediaPlayerContainer from '../react/containers/IIIFMediaPlayerContainer';
 
+document.addEventListener('turbolinks:before-cache', () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById('react-mounter'));
+});
+
 document.addEventListener('turbolinks:load', () => {
   ReactDOM.render(
     <IIIFMediaPlayerContainer />,
