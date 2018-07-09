c5a589cb599b359822dbfc3cbf530038e7a1c379
yingfeng-iu <yingfeng@iu.edu>
2018-07-09 16:07:42 -0400

    Issue#133: fix test

--- spec/helpers/hyrax_helper_spec.rb
+++ spec/helpers/hyrax_helper_spec.rb
@@ -1,10 +1,27 @@
-require 'rspec'
+# Copyright 2011-2018, The Trustees of Indiana University and Northwestern
+# University. Additional copyright may be held by others, as reflected in
+# the commit history.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ---  END LICENSE_HEADER BLOCK  ---
 
-RSpec.describe HyraxHelper, type: :helper do
-  describe "#capitalize_value" do
+require 'rails_helper'
+
+RSpec.describe HyraxHelper do
+  describe "#titleize_value" do
     let(:value) { 'test state' }
-    it "returns a string with the value capitalized" do
-      expect(helper.capitalize_value(value)).to eq('Test State')
+    it "returns a string with the value titleized" do
+      expect(helper.titleize_value(value)).to eq('Test State')
     end
   end
 end
