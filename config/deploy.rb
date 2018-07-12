1ebb3785c00f92e5a41cfb54bae75bed1e222747
Phil Dinh <phuongdh@gmail.com>
2018-07-12 15:14:15 -0400

    Keep only 5 releases now that we've fixed #174 (#190)

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :user, ENV['DEPLOY_USER'] || 'avalon'
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 20
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
