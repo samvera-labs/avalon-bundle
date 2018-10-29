d4fa5d4465900db439a0174eef12633fbf35e12f
Phuong Dinh <phuongdh@gmail.com>
2018-10-29 14:08:03 -0400

    Add Dockerfile. Work around for OSX volume

---
+++ docker/rails_init-dev.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Workaround for git clone problem when a non-existing uid:gid is used 
+export GIT_COMMITTER_NAME=avalon
+export GIT_COMMITTER_EMAIL=avalon@example.edu
+
+cd /home/app/avalon
+export HOME=/home/app/avalon
+
+echo " `date` : Bundle config"
+bundle config build.nokogiri --use-system-libraries
+
+echo " `date` : Bundle install"
+bundle install --with development test
+
+echo " `date` : Yarn install"
+yarn install
+
+echo " `date` : Remove server pid"
+rm -f tmp/pids/server.pid
+
+echo " `date` : Rake tasks"
+bundle exec rails db:migrate hyrax:default_collection_types:create hyrax:default_admin_set:create
+
+echo " `date` : Starting the rails server"
+bundle exec rails server -b 0.0.0.0 -p 3000 
+
+# Be able to restart rails server without killing the container
+tail -f /dev/null
