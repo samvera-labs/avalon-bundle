4eb910d7057f697c85b9ca7a361c6bac5f92b477
Phuong Dinh <phuongdh@gmail.com>
2019-02-07 16:54:58 -0500

    Support AWS ElasticTranscoder and CloudFront streaming

--- app/services/elastic_transcoder_option_service.rb
+++ app/services/elastic_transcoder_option_service.rb
@@ -5,31 +5,41 @@ class ElasticTranscoderOptionService
   # @param fileset the data to be persisted
   # @return an array of hashes of output options for audio/video
   def self.call(file_set)
-    # Defaults adapted from hydra-derivatives
-    audio_encoder = self.audio_encoder
+    file_name = file_set.label
+    options = { pipeline_id: Settings.aws.pipeline_id, masterfile_bucket: Settings.aws.masterfile_bucket }
 
     if file_set.audio?
-      [{ outputs = [{ key: "quality-medium/hls/#{file_name}", preset_id: self.find_or_create_preset('ts',:audio,:medium).id, segment_duration: '2' },
-                    { key: "quality-high/hls/#{file_name}", preset_id: self.find_or_create_preset('ts',:audio,:high).id, segment_duration: '2' } }]
+      options[:outputs] = [{ key: "quality-medium/hls/#{file_name}",
+                             preset_id: find_or_create_preset('ts', :audio, :medium).id,
+                             segment_duration: '2' },
+                           { key: "quality-high/hls/#{file_name}",
+                             preset_id: find_or_create_preset('ts', :audio, :high).id,
+                             segment_duration: '2' }]
     elsif file_set.video?
-      [{ outputs = [{ key: "quality-low/hls/#{file_name}", preset_id: self.find_or_create_preset('ts',:video,:low).id, segment_duration: '2' },
-                    { key: "quality-medium/hls/#{file_name}", preset_id: self.find_or_create_preset('ts',:video,:medium).id, segment_duration: '2' },
-                    { key: "quality-high/hls/#{file_name}", preset_id: self.find_or_create_preset('ts',:video,:high).id, segment_duration: '2' }] }]
-    else
-      []
+      options[:outputs] = [{ key: "quality-low/hls/#{file_name}",
+                             preset_id: find_or_create_preset('ts', :video, :low).id,
+                             segment_duration: '2' },
+                           { key: "quality-medium/hls/#{file_name}",
+                             preset_id: find_or_create_preset('ts', :video, :medium).id,
+                             segment_duration: '2' },
+                           { key: "quality-high/hls/#{file_name}",
+                             preset_id: find_or_create_preset('ts', :video, :high).id,
+                             segment_duration: '2' }]
     end
+
+    [options]
   end
 
   def self.client
     @client ||= Aws::ElasticTranscoder::Client.new
   end
 
-  def find_preset(container, format, quality)
+  def self.find_preset(container, format, quality)
     container_description = container == 'ts' ? 'hls' : container
     result = nil
     next_token = nil
     loop do
-      resp = self.client.list_presets page_token: next_token
+      resp = client.list_presets page_token: next_token
       result = resp.presets.find { |p| p.name == "avalon-#{format}-#{quality}-#{container_description}" }
       next_token = resp.next_page_token
       break if result.present? || next_token.nil?
@@ -37,30 +47,24 @@ class ElasticTranscoderOptionService
     result
   end
 
-  def read_preset(id)
-    self.client.read_preset(id: id).preset
+  def self.read_preset(id)
+    client.read_preset(id: id).preset
   end
 
-  def create_preset(container, format, quality)
-    self.client.create_preset(self.preset_settings(container, format, quality)).preset
+  def self.create_preset(container, format, quality)
+    client.create_preset(preset_settings(container, format, quality)).preset
   end
 
-  def find_or_create_preset(container, format, quality)
-    self.find_preset(container, format, quality) || self.create_preset(container, format, quality)
+  def self.find_or_create_preset(container, format, quality)
+    find_preset(container, format, quality) || create_preset(container, format, quality)
   end
 
   def self.preset_settings(container, format, quality)
-    templates = YAML.load(File.read(File.join(Rails.root,'config','encoding_presets.yml')))
+    templates = YAML.safe_load(File.read(Rails.root.join('config', 'encoding_presets.yml')))
     template = templates[:templates][format.to_sym].deep_dup.deep_merge(templates[:settings][format.to_sym][quality.to_sym])
     container_description = container == 'ts' ? 'hls' : container
-    template.merge!({
-      name: "avalon-#{format}-#{quality}-#{container_description}",
-      description: "Avalon Media System: #{format}/#{quality}/#{container_description}",
-      container: container
-    })
-  end
-
-  def self.audio_encoder
-    @audio_encoder ||= Hydra::Derivatives::AudioEncoder.new
+    template.merge!(name: "avalon-#{format}-#{quality}-#{container_description}",
+                    description: "Avalon Media System: #{format}/#{quality}/#{container_description}",
+                    container: container)
   end
 end
