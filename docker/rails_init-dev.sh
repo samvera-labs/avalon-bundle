bfa1e71c59b80146d87ab38fecbe7de882fc348b
Phuong Dinh <phuongdh@gmail.com>
2019-03-08 14:40:22 -0500

    Consolidate dev & test images, pass CircleCI

--- docker/rails_init-dev.sh
+++ docker/rails_init-dev.sh
@@ -10,7 +10,7 @@ echo " `date` : Bundle config"
 bundle config build.nokogiri --use-system-libraries
 
 echo " `date` : Bundle install"
-bundle install --with development test
+bundle install $BUNDLE_FLAGS 
 
 echo " `date` : Yarn install"
 # Workaround from https://github.com/yarnpkg/yarn/issues/2782
@@ -24,9 +24,3 @@ echo " `date` : Rake tasks"
 bundle exec rails db:migrate hyrax:default_collection_types:create hyrax:default_admin_set:create
 
 echo " `date` : Starting the rails server"
-bundle exec rails server -b 0.0.0.0 -p 3000 
-
-# WEBPACKER_DEV_SERVER_HOST=0.0.0.0 $NODE_PATH/.bin/webpack-dev-server --config /home/app/avalon/config/webpack/development.js
-
-# Be able to restart rails server without killing the container
-tail -f /dev/null
