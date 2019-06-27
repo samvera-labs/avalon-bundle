31c9cbde58ddb49c567efbc250e8bd39ad91a8da
Brian Keese <bkeese@indiana.edu>
2019-06-27 14:04:31 -0400

    Remove unneeded code.

--- app/controllers/active_encode_encode_records_controller.rb
+++ app/controllers/active_encode_encode_records_controller.rb
@@ -1,5 +1,5 @@
 class ActiveEncodeEncodeRecordsController < ApplicationController
-  before_action :set_active_encode_encode_record, only: [:show, :edit, :update, :destroy]
+  before_action :set_active_encode_encode_record, only: [:show]
 
   # GET /active_encode_encode_records
   # GET /active_encode_encode_records.json
@@ -12,63 +12,9 @@ class ActiveEncodeEncodeRecordsController < ApplicationController
   def show
   end
 
-  # GET /active_encode_encode_records/new
-  def new
-    @active_encode_encode_record = ::ActiveEncode::EncodeRecord.new
-  end
-
-  # GET /active_encode_encode_records/1/edit
-  def edit
-  end
-
-  # POST /active_encode_encode_records
-  # POST /active_encode_encode_records.json
-  def create
-    @active_encode_encode_record = ::ActiveEncode::EncodeRecord.new(active_encode_encode_record_params)
-
-    respond_to do |format|
-      if @active_encode_encode_record.save
-        format.html { redirect_to @active_encode_encode_record, notice: 'Active encode encode record was successfully created.' }
-        format.json { render :show, status: :created, location: @active_encode_encode_record }
-      else
-        format.html { render :new }
-        format.json { render json: @active_encode_encode_record.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /active_encode_encode_records/1
-  # PATCH/PUT /active_encode_encode_records/1.json
-  def update
-    respond_to do |format|
-      if @active_encode_encode_record.update(active_encode_encode_record_params)
-        format.html { redirect_to @active_encode_encode_record, notice: 'Active encode encode record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @active_encode_encode_record }
-      else
-        format.html { render :edit }
-        format.json { render json: @active_encode_encode_record.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /active_encode_encode_records/1
-  # DELETE /active_encode_encode_records/1.json
-  def destroy
-    @active_encode_encode_record.destroy
-    respond_to do |format|
-      format.html { redirect_to active_encode_encode_records_url, notice: 'Active encode encode record was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_active_encode_encode_record
       @active_encode_encode_record = ::ActiveEncode::EncodeRecord.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def active_encode_encode_record_params
-      params.require(:active_encode_encode_record).permit(:global_id, :state, :adapter, :title, :raw_object)
     end
 end
--- spec/controllers/active_encode_encode_records_controller_spec.rb
+++ spec/controllers/active_encode_encode_records_controller_spec.rb
@@ -25,25 +25,17 @@ require 'rails_helper'
 
 RSpec.describe ActiveEncodeEncodeRecordsController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # ActiveEncodeEncodeRecord. As you add validations to ActiveEncodeEncodeRecord, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # ActiveEncodeEncodeRecordsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
+  before do
+    let(:active_encode_encode_record) { FactoryBot.create(:active_encode_encode_record) }
+  end
+
   describe "GET #index" do
     it "returns a success response" do
-      ActiveEncodeEncodeRecord.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(response).to be_successful
     end
@@ -51,91 +43,8 @@ RSpec.describe ActiveEncodeEncodeRecordsController, type: :controller do
 
   describe "GET #show" do
     it "returns a success response" do
-      active_encode_encode_record = ActiveEncodeEncodeRecord.create! valid_attributes
       get :show, params: {id: active_encode_encode_record.to_param}, session: valid_session
       expect(response).to be_successful
     end
   end
-
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns a success response" do
-      active_encode_encode_record = ActiveEncodeEncodeRecord.create! valid_attributes
-      get :edit, params: {id: active_encode_encode_record.to_param}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new ActiveEncodeEncodeRecord" do
-        expect {
-          post :create, params: {active_encode_encode_record: valid_attributes}, session: valid_session
-        }.to change(ActiveEncodeEncodeRecord, :count).by(1)
-      end
-
-      it "redirects to the created active_encode_encode_record" do
-        post :create, params: {active_encode_encode_record: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(ActiveEncodeEncodeRecord.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {active_encode_encode_record: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested active_encode_encode_record" do
-        active_encode_encode_record = ActiveEncodeEncodeRecord.create! valid_attributes
-        put :update, params: {id: active_encode_encode_record.to_param, active_encode_encode_record: new_attributes}, session: valid_session
-        active_encode_encode_record.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "redirects to the active_encode_encode_record" do
-        active_encode_encode_record = ActiveEncodeEncodeRecord.create! valid_attributes
-        put :update, params: {id: active_encode_encode_record.to_param, active_encode_encode_record: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(active_encode_encode_record)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
-        active_encode_encode_record = ActiveEncodeEncodeRecord.create! valid_attributes
-        put :update, params: {id: active_encode_encode_record.to_param, active_encode_encode_record: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested active_encode_encode_record" do
-      active_encode_encode_record = ActiveEncodeEncodeRecord.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: active_encode_encode_record.to_param}, session: valid_session
-      }.to change(ActiveEncodeEncodeRecord, :count).by(-1)
-    end
-
-    it "redirects to the active_encode_encode_records list" do
-      active_encode_encode_record = ActiveEncodeEncodeRecord.create! valid_attributes
-      delete :destroy, params: {id: active_encode_encode_record.to_param}, session: valid_session
-      expect(response).to redirect_to(active_encode_encode_records_url)
-    end
-  end
-
 end
