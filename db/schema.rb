29bb033346e671fa097d3dbd9f54347b14949ee8
phuongdh <phuongdh@gmail.com>
2020-05-26 11:13:07 -0400

    Fix docker dev setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190723195828) do
+ActiveRecord::Schema.define(version: 2019_07_23_195828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
